fe26b840a2f61464642deb650c02b551054c8f4d
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-03-01 17:06:42 +0900

    spotモデルのバリデーション実装

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,9 @@
 class Spot < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true, numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :state_id
+    validates :spot_type_id
+  end
+  validates :city, presence: true
 end
