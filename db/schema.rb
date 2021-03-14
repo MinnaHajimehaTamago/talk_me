6182b0c780bacb63eed9722094d2b9a7691bb647
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-03-14 23:40:13 +0900

    appearamcesモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_072213) do
+ActiveRecord::Schema.define(version: 2021_03_14_143312) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,24 @@ ActiveRecord::Schema.define(version: 2021_03_13_072213) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "appearances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "hair_id", null: false
+    t.integer "eye_shape_id", null: false
+    t.integer "eye_size_id", null: false
+    t.integer "nose_shape_id", null: false
+    t.integer "nose_size_id", null: false
+    t.integer "mouth_shape_id", null: false
+    t.integer "mouth_size_id", null: false
+    t.integer "height_id", null: false
+    t.integer "body_shape_id", null: false
+    t.integer "gender_id", null: false
+    t.string "image", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_appearances_on_user_id"
   end
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -111,6 +129,7 @@ ActiveRecord::Schema.define(version: 2021_03_13_072213) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "appearances", "users"
   add_foreign_key "favorites", "signs"
   add_foreign_key "favorites", "users"
   add_foreign_key "signs", "users"
