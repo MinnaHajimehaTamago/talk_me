b9d821aa1ad61eecbe0b5becddc229c8705599a7
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-11 23:44:50 +0900

    トーク機能の修正

--- app/javascript/packs/message_ajax.js
+++ app/javascript/packs/message_ajax.js
@@ -1,44 +1,58 @@
 if (location.pathname.match("rooms") && location.pathname.match("messages")) {
   document.addEventListener('DOMContentLoaded', function() {
-    const reloadBtn = document.getElementById('reload_btn');
     const userId = $('.user-id').val();
     const displayMessages = document.getElementById('messages');
+    let currentMessagesLength = displayMessages.childElementCount;
     function messagesReload() {
       const XHR = new XMLHttpRequest();
       XHR.open("GET", `${location.pathname}/reload`, true);
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
-        displayMessages.innerHTML = "";
         const messages = XHR.response.messages;
-        messages.forEach((message) => {
-          const messageWrap = document.createElement("div");
-          messageWrap.setAttribute("class", "message-wrap");
-          const messageWrapChild = document.createElement("div");
-          if (message.user_id == userId) {
-            messageWrapChild.setAttribute("class", "message-wrap-right");
-          } else {
-            messageWrapChild.setAttribute("class", "message-wrap-left");
-          };
-          const messageElement = document.createElement("p");
-          messageElement.setAttribute("class", "message");
-          messageElement.innerHTML = message.text;
-          const messageTime = document.createElement("p");
-          messageTime.setAttribute("class", "message-time");
-          messageTime.innerHTML = message.created_at;
-          messageWrap.appendChild(messageWrapChild);
-          if (message.user_id == userId) {
-            messageWrapChild.appendChild(messageTime);
-            messageWrapChild.appendChild(messageElement);
-          } else {
-            messageWrapChild.appendChild(messageElement);
-            messageWrapChild.appendChild(messageTime);
-          };
-          displayMessages.appendChild(messageWrap);
-          displayMessages.scrollTop = displayMessages .scrollHeight;
-        });
+        messageLengthDifference = messages.length - currentMessagesLength;
+        if (messages.length != currentMessagesLength) {
+          messages.slice(messages.length - 1).forEach((message) => {
+            const messageWrap = document.createElement("div");
+            messageWrap.setAttribute("class", "message-wrap");
+            const messageWrapChild = document.createElement("div");
+            if (message.user_id == userId) {
+              messageWrapChild.setAttribute("class", "message-wrap-right");
+            } else {
+              messageWrapChild.setAttribute("class", "message-wrap-left");
+            };
+            const messageElement = document.createElement("p");
+            messageElement.setAttribute("class", "message");
+            messageElement.innerHTML = message.text;
+            const messageTime = document.createElement("p");
+            messageTime.setAttribute("class", "message-time");
+            messageTime.innerHTML = createdAtFormat(message.created_at);
+            messageWrap.appendChild(messageWrapChild);
+            if (message.user_id == userId) {
+              messageWrapChild.appendChild(messageTime);
+              messageWrapChild.appendChild(messageElement);
+            } else {
+              messageWrapChild.appendChild(messageElement);
+              messageWrapChild.appendChild(messageTime);
+            };
+            displayMessages.appendChild(messageWrap);
+            displayMessages.scrollTop = displayMessages .scrollHeight;
+            currentMessagesLength = messages.length;
+          });
+        };
       };
     };
-    setInterval(messagesReload, 1000);
+
+    function createdAtFormat(time) {
+      time = time.toString()
+      month = time.substr(5, 2);
+      day = time.substr(8, 2);
+      hour = time.substr(11, 2);
+      minute = time.substr(14, 2);
+      display = `${month}/${day} ${hour}:${minute}`;
+      return display;
+    };
+    
+    setInterval(messagesReload, 5000);
   });
 };
--- app/javascript/packs/sign_tag_input.js
+++ app/javascript/packs/sign_tag_input.js
@@ -5,110 +5,112 @@ if (location.pathname.match("signs")) {
     const addBtn = document.getElementById('tag_add_btn');
     let tagCount = 0;
 
-    addBtn.addEventListener('click', () => {
-      if (inputElement.value != "") {
-        const registrationTag = document.createElement('div');
-        registrationTag.setAttribute('id', "tag_element");
-        registrationTag.setAttribute('class', 'tag-element');
-        const deleteBtn = document.createElement('i');
-        deleteBtn.setAttribute('class', "far fa-times-circle tag-delete-btn");
-        deleteBtn.setAttribute('id', "tag_delete_btn");
-        registrationTag.appendChild(deleteBtn);
-        tagText = document.createElement('div');
-        tagText.innerHTML = inputElement.value;
-        registrationTag.appendChild(tagText);
-        let registrationTagNum = tagCount;
-        const inputHTML = document.createElement('input');
-        inputHTML.setAttribute('id', `signs_tag_${registrationTagNum}`);
-        inputHTML.setAttribute('name', 'signs_tag[names][]');
-        inputHTML.setAttribute('type', 'text');
-        inputHTML.setAttribute('class', 'hide');
-        registrationTag.appendChild(inputHTML);
-        TagLists.appendChild(registrationTag);
-        let registrationTagInput = document.getElementById(`signs_tag_${registrationTagNum}`);
-        registrationTagInput.value = inputElement.value;
-        addBtn.style.backgroundColor = 'lightgray';
-        addBtn.style.cursor = 'default'
-        inputElement.value = "";
-        const searchResult = document.getElementById("search-result");
-        searchResult.innerHTML = "";
-        tagCount += 1;
-      };
-    });
+    const signsTagText = document.getElementById('signs_tag_text');
+    if (signsTagText != null) {
+      addBtn.addEventListener('click', () => {
+        if (inputElement.value != "") {
+          const registrationTag = document.createElement('div');
+          registrationTag.setAttribute('id', "tag_element");
+          registrationTag.setAttribute('class', 'tag-element');
+          const deleteBtn = document.createElement('i');
+          deleteBtn.setAttribute('class', "far fa-times-circle tag-delete-btn");
+          deleteBtn.setAttribute('id', "tag_delete_btn");
+          registrationTag.appendChild(deleteBtn);
+          tagText = document.createElement('div');
+          tagText.innerHTML = inputElement.value;
+          registrationTag.appendChild(tagText);
+          let registrationTagNum = tagCount;
+          const inputHTML = document.createElement('input');
+          inputHTML.setAttribute('id', `signs_tag_${registrationTagNum}`);
+          inputHTML.setAttribute('name', 'signs_tag[names][]');
+          inputHTML.setAttribute('type', 'text');
+          inputHTML.setAttribute('class', 'hide');
+          registrationTag.appendChild(inputHTML);
+          TagLists.appendChild(registrationTag);
+          let registrationTagInput = document.getElementById(`signs_tag_${registrationTagNum}`);
+          registrationTagInput.value = inputElement.value;
+          addBtn.style.backgroundColor = 'lightgray';
+          addBtn.style.cursor = 'default'
+          inputElement.value = "";
+          const searchResult = document.getElementById("search-result");
+          searchResult.innerHTML = "";
+          tagCount += 1;
+        };
+      });
 
-    inputElement.addEventListener("keyup", () => {
-      const keyword = document.getElementById("signs_tag_input").value;
-      const XHR = new XMLHttpRequest();
-      XHR.open("GET", `../tags/search/?keyword=${keyword}`, true);
-      XHR.responseType = "json";
-      XHR.send();
-      XHR.onload = () => {
-        const searchResult = document.getElementById("search-result");
-        searchResult.innerHTML = "";
-        if (XHR.response) {
-          const tagName = XHR.response.keyword;
-          tagName.forEach((tag) => {
-            const childElement = document.createElement("div");
-            childElement.setAttribute("class", "tag-candidate");
-            childElement.setAttribute("id", tag.id);
-            childElement.innerHTML = tag.name;
-            if (tag.officiality_id == 1) {
-              const officialMark = document.createElement("img");
-              officialMark.src = "/assets/official_mark.png";
-              childElement.appendChild(officialMark);
-            };
-            searchResult.appendChild(childElement);
-            const clickElement = document.getElementById(tag.id);
-            clickElement.addEventListener("click", () => {
-              const registrationTag = document.createElement('div');
-              registrationTag.setAttribute('id', "tag_element");
-              registrationTag.setAttribute('class', 'tag-element');
-              let registrationTagNum = tagCount;
-              const inputHTML = document.createElement('input');
-              inputHTML.setAttribute('id', `signs_tag_${registrationTagNum}`);
-              inputHTML.setAttribute('name', 'signs_tag[names][]');
-              inputHTML.setAttribute('type', 'text');
-              inputHTML.setAttribute('class', 'hide');
-              const deleteBtn = document.createElement('i');
-              deleteBtn.setAttribute('class', "far fa-times-circle tag-delete-btn");
-              deleteBtn.setAttribute('id', "tag_delete_btn");
-              registrationTag.appendChild(deleteBtn);
-              tagText = document.createElement('div');
-              tagText.innerHTML = tag.name;
-              registrationTag.appendChild(tagText);
+      inputElement.addEventListener("keyup", () => {
+        const keyword = document.getElementById("signs_tag_input").value;
+        const XHR = new XMLHttpRequest();
+        XHR.open("GET", `../tags/search/?keyword=${keyword}`, true);
+        XHR.responseType = "json";
+        XHR.send();
+        XHR.onload = () => {
+          const searchResult = document.getElementById("search-result");
+          searchResult.innerHTML = "";
+          if (XHR.response) {
+            const tagName = XHR.response.keyword;
+            tagName.forEach((tag) => {
+              const childElement = document.createElement("div");
+              childElement.setAttribute("class", "tag-candidate");
+              childElement.setAttribute("id", tag.id);
+              childElement.innerHTML = tag.name;
               if (tag.officiality_id == 1) {
                 const officialMark = document.createElement("img");
                 officialMark.src = "/assets/official_mark.png";
-                registrationTag.appendChild(officialMark);
+                childElement.appendChild(officialMark);
               };
-              registrationTag.appendChild(inputHTML);
-              TagLists.appendChild(registrationTag);
-              let registrationTagInput = document.getElementById(`signs_tag_${registrationTagNum}`);
-              registrationTagInput.value = tag.name          
-              inputElement.value = "";
-              searchResult.innerHTML = "";
-              addBtn.style.backgroundColor = 'lightgray';
-              addBtn.style.cursor = 'default'
-              tagCount += 1;
+              searchResult.appendChild(childElement);
+              const clickElement = document.getElementById(tag.id);
+              clickElement.addEventListener("click", () => {
+                const registrationTag = document.createElement('div');
+                registrationTag.setAttribute('id', "tag_element");
+                registrationTag.setAttribute('class', 'tag-element');
+                let registrationTagNum = tagCount;
+                const inputHTML = document.createElement('input');
+                inputHTML.setAttribute('id', `signs_tag_${registrationTagNum}`);
+                inputHTML.setAttribute('name', 'signs_tag[names][]');
+                inputHTML.setAttribute('type', 'text');
+                inputHTML.setAttribute('class', 'hide');
+                const deleteBtn = document.createElement('i');
+                deleteBtn.setAttribute('class', "far fa-times-circle tag-delete-btn");
+                deleteBtn.setAttribute('id', "tag_delete_btn");
+                registrationTag.appendChild(deleteBtn);
+                tagText = document.createElement('div');
+                tagText.innerHTML = tag.name;
+                registrationTag.appendChild(tagText);
+                if (tag.officiality_id == 1) {
+                  const officialMark = document.createElement("img");
+                  officialMark.src = "/assets/official_mark.png";
+                  registrationTag.appendChild(officialMark);
+                };
+                registrationTag.appendChild(inputHTML);
+                TagLists.appendChild(registrationTag);
+                let registrationTagInput = document.getElementById(`signs_tag_${registrationTagNum}`);
+                registrationTagInput.value = tag.name          
+                inputElement.value = "";
+                searchResult.innerHTML = "";
+                addBtn.style.backgroundColor = 'lightgray';
+                addBtn.style.cursor = 'default'
+                tagCount += 1;
+              });
             });
-          });
+          };
         };
-      };
 
-      if (inputElement.value != "") {
-        addBtn.style.backgroundColor = '#80a8ff';
-        addBtn.style.cursor = 'pointer'
-      } else {
-        addBtn.style.backgroundColor = 'lightgray';
-        addBtn.style.cursor = 'default';
-      };
-    });
-
-    $('#tag_lists').on('mouseover', function (){
-      $('#tag_lists #tag_delete_btn').on('click', function () {
-        $(this).parent().remove();
+        if (inputElement.value != "") {
+          addBtn.style.backgroundColor = '#80a8ff';
+          addBtn.style.cursor = 'pointer'
+        } else {
+          addBtn.style.backgroundColor = 'lightgray';
+          addBtn.style.cursor = 'default';
+        };
       });
-    });
 
+      $('#tag_lists').on('mouseover', function (){
+        $('#tag_lists #tag_delete_btn').on('click', function () {
+          $(this).parent().remove();
+        });
+      });
+    };
   });
 };
