7b147e8af48b5702ff23da6e373509429231627d
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-14 08:08:56 +0900

    トップページの修正

--- app/javascript/packs/app_explanation.js
+++ app/javascript/packs/app_explanation.js
@@ -1,33 +1,64 @@
-$(function() {
-  $('.app-explanation-container').each(function() {
-    var $explanationWraps = $(this).find('.app-explanation-wrap'),
-        $explanationBg = $('.app-explanation-bg'),
-        $explanationContainer = $('.app-explanation-container'),
-        currentIndex = 0;
+// $(function() {
+//   $('.app-explanation-container').each(function() {
+//     var $explanationWraps = $(this).find('.app-explanation-wrap'),
+//         $explanationBg = $('.app-explanation-bg'),
+//         $explanationContainer = $('.app-explanation-container'),
+//         currentIndex = 0;
 
-    $('.app-explanation-btn').on('click', function() {
-      $explanationContainer.addClass('active');
-      $explanationBg.addClass('active');
-      $explanationWraps.eq(currentIndex).addClass('active');
+//     $('.app-explanation-btn').on('click', function() {
+//       $explanationContainer.addClass('active');
+//       $explanationBg.addClass('active');
+//       $explanationWraps.eq(currentIndex).addClass('active');
+//     });
+
+//     $('.app-explanation-next-btn').on('click', function() {
+//       $explanationWraps.eq(currentIndex).removeClass('active');
+//       $explanationWraps.eq(currentIndex + 1).addClass('active');
+//       currentIndex += 1;
+//     });
+
+//     $('.app-explanation-back-btn').on('click', function() {
+//       $explanationWraps.eq(currentIndex).removeClass('active');
+//       $explanationWraps.eq(currentIndex - 1).addClass('active');
+//       currentIndex -= 1;
+//     });
+
+//     $('.modal-close-icon').on('click', function() {
+//       $explanationBg.removeClass('active');
+//       $explanationContainer.removeClass('active');
+//       $explanationWraps.eq(currentIndex).removeClass('active');
+//       currentIndex = 0;
+//     });
+//   });
+// });
+
+$(function () {
+  $('#app_explanation_btn').each(function () {
+    var $wrap = $('.app-explanation-wrap'),
+        $wrapOffsetTop = $wrap.offset().top,
+        el = scrollableElement('html', 'body');
+    
+    $(this).on('click', function () {
+      $(el).animate({ scrollTop: $wrapOffsetTop}, 250);
     });
 
-    $('.app-explanation-next-btn').on('click', function() {
-      $explanationWraps.eq(currentIndex).removeClass('active');
-      $explanationWraps.eq(currentIndex + 1).addClass('active');
-      currentIndex += 1;
-    });
-
-    $('.app-explanation-back-btn').on('click', function() {
-      $explanationWraps.eq(currentIndex).removeClass('active');
-      $explanationWraps.eq(currentIndex - 1).addClass('active');
-      currentIndex -= 1;
-    });
-
-    $('.modal-close-icon').on('click', function() {
-      $explanationBg.removeClass('active');
-      $explanationContainer.removeClass('active');
-      $explanationWraps.eq(currentIndex).removeClass('active');
-      currentIndex = 0;
-    });
+    function scrollableElement(){
+      var i, len, el, $el, scrollable;
+      for ( i = 0, len = arguments.length; i < len; i++){
+          el = arguments[i],
+          $el = $(el);
+          if ($el.scrollTop() > 0){
+              return el;
+          } else {
+              $el.scrollTop(1);
+              scrollable = $el.scrollTop() > 0;
+              $el.scrollTop(0);
+              if (scrollable){
+                  return el;
+              }
+          }
+      }
+      return [];
+  }
   });
 });
