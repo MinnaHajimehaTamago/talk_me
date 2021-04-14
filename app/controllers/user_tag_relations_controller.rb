f31d71fb853522738f1dbddf303064120632b607
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-15 08:43:37 +0900

    アプリ全体のview調整

--- app/controllers/user_tag_relations_controller.rb
+++ app/controllers/user_tag_relations_controller.rb
@@ -2,7 +2,7 @@ class UserTagRelationsController < ApplicationController
   def destroy
     user_tag = UserTagRelation.find(params[:id])
     user_tag.destroy
-    if params[:path].include?("/tags/after_sign_up")
+    if params[:path].present? && params[:path].include?("/tags/after_sign_up")
       redirect_to after_sign_up_new_tags_path
     else
       redirect_to new_tag_path
