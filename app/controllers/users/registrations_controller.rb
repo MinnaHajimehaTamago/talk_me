43b6c1d602e571056156eee244f063bb3e4cfb01
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-20 17:40:18 +0900

    サインアップ後の遷移先修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    new_tag_path(resource)
+    after_sign_up_new_tags_path(resource)
   end
 
   # The path used after sign up for inactive accounts.
