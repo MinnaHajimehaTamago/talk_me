3a102fe36eff35f3fbea012c64dd8ff8229b8ccd
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-22 23:01:33 +0900

    不要なコードの削除

--- app/models/spot.rb
+++
@@ -1,9 +0,0 @@
-class Spot < ApplicationRecord
-  belongs_to :user
-
-  with_options presence: true, numericality: { other_than: 1, message: 'を選択してください' } do
-    validates :state_id
-    validates :spot_type_id
-  end
-  validates :city, presence: true
-end
