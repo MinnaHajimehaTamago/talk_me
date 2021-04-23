d2ed7d3b11bd24565cbee24933ee080774115ef6
MinnaHajimehaTamago <74609389+MinnaHajimehaTamago@users.noreply.github.com>
2021-04-23 23:22:20 +0900

    sigsモデルテストの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
     email                 { 'test1@example.com' }
     password              { 'password' }
     password_confirmation { password }
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/kumao0.png'), filename: 'kumao0.png')
+    end
   end
 end
--- spec/model/signs_spec.rb
+++ spec/model/signs_spec.rb
@@ -13,100 +13,10 @@ describe Sign, type: :model do
     end
 
     context 'サインが登録できない時' do
-      it 'first_nameが空のでは登録できないこと' do
-        @sign.first_name = nil
+      it 'textが入力されていないと登録できないこと' do
+        @sign.text = nil
         @sign.valid?
-        expect(@sign.errors.full_messages).to include('姓（全角）を入力してください')
-      end
-      it 'first_nameが漢字・ひらがな・カタカナ（全角）以外では登録できないこと' do
-        @sign.first_name = 'nameﾅﾏｴ'
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('姓（全角）は全角（漢字・ひらがな・カタカナ）で入力してください')
-      end
-      it 'last_nameが空では登録できないこと' do
-        @sign.last_name = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('名（全角）を入力してください')
-      end
-      it 'last_nameが漢字・ひらがな・カタカナ（全角）以外では登録できないこと' do
-        @sign.last_name = 'nameﾅﾏｴ'
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('名（全角）は全角（漢字・ひらがな・カタカナ）で入力してください')
-      end
-      it 'first_name_kanaが空では登録できないこと' do
-        @sign.first_name_kana = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('姓（全角カナ）を入力してください')
-      end
-      it 'first_name_kanaがカタカナ以外では登録できないこと' do
-        @sign.first_name_kana = 'name'
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('姓（全角カナ）はカタカナで入力してください')
-      end
-      it 'last_name_kanaが空では登録できないこと' do
-        @sign.last_name_kana = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('名（全角カナ）を入力してください')
-      end
-      it 'last_name_kanaがカタカナ以外では登録できないこと' do
-        @sign.last_name_kana = 'name'
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('名（全角カナ）はカタカナで入力してください')
-      end
-      it 'state_idが空では登録できないこと' do
-        @sign.state_id = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('都道府県を選択してください')
-      end
-      it 'state_idが1では登録できないこと' do
-        @sign.state_id = 1
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('都道府県を選択してください')
-      end
-      it 'cityが空では登録できないこと' do
-        @sign.city = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('市区町村を入力してください')
-      end
-      it 'spot_type_idが空では登録できないこと' do
-        @sign.spot_type_id = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('場所の特徴を選択してください')
-      end
-      it 'spot_type_idが1では登録できないこと' do
-        @sign.spot_type_id = 1
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('場所の特徴を選択してください')
-      end
-      it 'position_idが空では登録できないこと' do
-        @sign.position_id = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('「どこに」を選択してください')
-      end
-      it 'position_idが1では登録できないこと' do
-        @sign.position_id = 1
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('「どこに」を選択してください')
-      end
-      it 'characteristic_idが空では登録できないこと' do
-        @sign.characteristic_id = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('「何を」を選択してください')
-      end
-      it 'characteristic_idが1では登録できないこと' do
-        @sign.characteristic_id = 1
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('「何を」を選択してください')
-      end
-      it 'content_idが空では登録できないこと' do
-        @sign.content_id = nil
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('サインの内容を選択してください')
-      end
-      it 'content_idが1では登録できないこと' do
-        @sign.content_id = 1
-        @sign.valid?
-        expect(@sign.errors.full_messages).to include('サインの内容を選択してください')
+        expect(@sign.errors.full_messages).to include('メッセージを入力してください')
       end
     end
   end
